52b5298657e3c464095feb4771338f16fbce6e50
Stepan <stepnikoghosyan@gmail.com>
2022-12-28 23:56:13 +0400

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+// models
+import { EnvConfig, EnvConfigEnum } from './common/interfaces/env-config.model';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  // Swagger
+  const config = new DocumentBuilder().setTitle('Blog API').setVersion('1.0').addBearerAuth().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
+  await app.listen(configService.get<EnvConfig[EnvConfigEnum.PORT]>(EnvConfigEnum.PORT) || process.env.PORT);
 }
 bootstrap();
